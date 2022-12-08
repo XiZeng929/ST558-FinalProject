89082b3c01b2451cc469cd71a483ad5d48e15c4d
Xi Zeng <xizeng929@gmail.com>
2022-12-07 23:55:48 -0500

    Dataset and image upload as well as About Page in ui

--- server.R
+++ server.R
@@ -1,17 +1,21 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#ST558 Final Project
+#Xi Zeng
 
+#Packaged used
 library(shiny)
+library(tidyverse)
+library(DT)
+library(readxl)
+
+#Read in data
+raisin <- read_excel("Raisin_Dataset.xlsx", sheet = "Raisin_Grains_Dataset")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+    output$image <- renderImage({
+        list(src = "")
+    } ,deleteFile = FALSE)
     output$distPlot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
--- ui.R
+++ ui.R
@@ -1,33 +1,66 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#ST558 Final Project
+#Xi Zeng
 
+#Packaged used
 library(shiny)
+library(tidyverse)
+library(DT)
+library(readxl)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+#Read in data
+raisin <- read_excel("Raisin_Dataset.xlsx", sheet = "Raisin_Grains_Dataset")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+#Define UI output
+header <- dashboardHeader(title = "Final Project")
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("About", tabName = "desc"),
+        menuItem("Data Exploration", tabName = "explore"),
+        menuItem("Modeling ", tabName = "model",
+                 menuSubItem("Modeling Info", tabName = "info"),
+                 menuSubItem("Model Fitting", tabName = "fitting"),
+                 menuSubItem("Prediction", tabName = "pred")),
+        menuItem("Data", tabName = "data")
+    )
+)
+
+body <- dashboardBody(
+    tabItems(
+        tabItem(tabName = "desc",
+                h2("Data Description"),
+                br(),
+                h3("Purpose"),
+                br(),
+                "This App is used for manipulating data and model fitting, and also related to utility of R shiny",
+                br(),
+                h3("Data"),
+                br(),
+                "The data comes from UCI Machine Learning Repository. ",
+                a("(Data Source here) ",href = "https://archive.ics.uci.edu/ml/datasets/Raisin+Dataset#"),
+                "It has a binary response, and 7 continuous predictors, and it has total of 900 observations.",
+                br(),
+                "Here, the application has total of 4 pages. The About page gives an introduction of the project as well as the dataset. The Data Exploration page can enable creating numerical and graphical summares as well as some user-defined option for data and plots. In Model Fitting page, 3 supervised learning model will be utilized to model the data. At last, the Data page enables user to subset and save data.",  
+                "∗ Include a picture related to the data (for instance, if the data was about the world wildlife fund, you might include a picture of their logo)",
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        tabItem(tabName = "explore",
+                h2("Data Exploration")
+        ),
+        
+        tabItem(tabName = "model",
+                h2("Modelling")
+        ),
+        tabItem(tabName = "data",
+                h2("Data glance and subsetting")
         )
     )
-))
+)
+
+
+dashboardPage(
+    header,
+    sidebar,
+    body
+)
